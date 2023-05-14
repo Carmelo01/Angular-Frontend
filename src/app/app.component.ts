3acab05398879fa64f67030c9f29919161d7e130
Carmelo01 <franciscarmelo123@gmail.com>
2023-05-15 02:12:24 +0800

    May 15, 2023 2:12AM export pdf for graded capsule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenService } from './services/token.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
-
-
+export class AppComponent implements OnInit{
+  currentUser: any;
+  tokenGet: any;
+  constructor (private token: TokenService){}
+  ngOnInit(): void {
+    this.tokenGet = this.token.get()
+    if(this.tokenGet){
+      if(this.tokenGet.role == "admin"){
+        this.token.adminMe().subscribe({
+          next: data => this.currentUser = data,
+          error: error => this.handleError(error)
+        })
+      } else {
+        this.token.me().subscribe({
+          next: data => this.currentUser = data,
+          error: error => this.handleError(error)
+        })
+      }
+    }
+  }
+  handleError(error: any){
+    this.token.remove();
+    window.location.reload();
+  }
 }
